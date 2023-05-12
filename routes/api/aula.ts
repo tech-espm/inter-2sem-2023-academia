10cbc61087df779d4c2f1ad2a0285ffeafff36a0
joaopedrocaldas <jpecaldas@gmail.com>
2023-05-12 12:51:13 -0300

    commit do brabo

---
+++ routes/api/aula.ts
@@ -0,0 +1,13 @@
+﻿import app = require("teem");
+
+class AulaRoute {
+	@app.http.post()
+	@app.route.formData()
+	public async criar(req: app.Request, res: app.Response) {
+		let pessoa = req.body;
+
+		res.json("OK!");
+	}
+}
+
+export = AulaRoute;
